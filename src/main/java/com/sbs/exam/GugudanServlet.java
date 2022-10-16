b744387a23fc54a835c7aa0c4491dcd37b089006
JY <egoista890@gmail.com>
2022-10-16 18:10:50 +0900

    ãJSP에서 Rq 이용

--- src/main/java/com/sbs/exam/GugudanServlet.java
+++ src/main/java/com/sbs/exam/GugudanServlet.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.sbs.exam;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,10 +14,10 @@ public class GugudanServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     Rq rq = new Rq(req, resp);
 
-    int dan = rq.getIntParam("dan", 0);
-    int limit = rq.getIntParam("limit", 0);
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
 
-    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+    rq.appendBody("<div class=\"a\"></div>\n".formatted(dan));
     for (int i = 1; i <= limit; i++) {
       rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
     }
